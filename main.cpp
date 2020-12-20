fb12c3dd06898211b245ba7ad54dca3cabf08858
River <524429848@qq.com>
2020-12-20 18:47:09 +0800

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+#include"class_student.h"
+#include"functions.h"
+using namespace std;
+using namespace data;
+
+int main()
+{
+    init();
+    
+    return 0;
+}
